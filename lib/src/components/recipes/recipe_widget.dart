45237af789d12610e388429f8a17c51e0027436e
Rashid <rkhabeer84@gmail.com>
2022-05-30 23:36:40 +0500

    Bookmark action

--- lib/src/components/recipes/recipe_widget.dart
+++ lib/src/components/recipes/recipe_widget.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_recipie_app/src/base/assets.dart';
 import 'package:food_recipie_app/src/base/nav.dart';
 import 'package:food_recipie_app/src/base/themes.dart';
 import 'package:food_recipie_app/src/components/recipes/recipe_detail_page.dart';
 import 'package:food_recipie_app/src/data/models.dart';
 import 'package:food_recipie_app/src/services/app_firestore_service.dart';
+import 'package:food_recipie_app/src/services/firebase_auth_service.dart';
 import 'package:food_recipie_app/src/utils/const.dart';
 import 'package:food_recipie_app/src/widgets/blur_widget.dart';
 import 'package:food_recipie_app/src/widgets/network_image_widget.dart';
 import 'package:food_recipie_app/src/widgets/show_rating_widget.dart';
 import 'package:food_recipie_app/src/widgets/simple_stream_builder.dart';
 
-class RecipeWidget extends StatelessWidget {
+class RecipeWidget extends StatefulWidget {
   const RecipeWidget({
     Key? key,
     required this.recipe,
@@ -24,19 +26,26 @@ class RecipeWidget extends StatelessWidget {
   final EdgeInsets padding;
 
   @override
+  State<RecipeWidget> createState() => _RecipeWidgetState();
+}
+
+class _RecipeWidgetState extends State<RecipeWidget> {
+  final _userId = FirebaseAuthService.userId;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: padding,
+      padding: widget.padding,
       child: GestureDetector(
         onTap: () {
-          AppNavigation.to(context, RecipeDetailPage(recipe: recipe));
+          AppNavigation.to(context, RecipeDetailPage(recipe: widget.recipe));
         },
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              width: width ?? double.infinity,
+              width: widget.width ?? double.infinity,
               height: 180,
               margin: const EdgeInsets.only(bottom: 12),
               decoration: BoxDecoration(
@@ -45,18 +54,41 @@ class RecipeWidget extends StatelessWidget {
               child: Stack(children: [
                 ClipRRect(
                   borderRadius: BorderRadius.circular(10),
-                  child: NetworkImageWidget(url: recipe.imagesList.first),
+                  child:
+                      NetworkImageWidget(url: widget.recipe.imagesList.first),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8),
                   child: Column(children: [
-                    ShowRatingWidget(rating: recipe.rating),
+                    Row(children: [
+                      ShowRatingWidget(rating: widget.recipe.rating),
+                      const Spacer(),
+                      TextButton(
+                        onPressed: _bookMarkAction,
+                        child: Image.asset(
+                          AppAssets.saved,
+                          color: widget.recipe.savedUsersIds.contains(_userId)
+                              ? AppTheme.primaryColor.shade500
+                              : const Color(0xff130F26),
+                          height: 14.61,
+                          width: 18.22,
+                        ),
+                        style: TextButton.styleFrom(
+                          minimumSize: const Size(32, 32),
+                          primary: AppTheme.primaryColor.shade500,
+                          backgroundColor: Colors.white,
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          elevation: 5,
+                          shape: const CircleBorder(),
+                        ),
+                      ),
+                    ]),
                     const Spacer(),
                     Align(
                       alignment: Alignment.centerRight,
                       child: BlurWidget(
                         child: Text(
-                          recipe.cookingTime,
+                          widget.recipe.cookingTime,
                           style: const TextStyle(
                             fontWeight: FontWeight.w400,
                             fontSize: 12,
@@ -69,13 +101,13 @@ class RecipeWidget extends StatelessWidget {
                 ),
               ], fit: StackFit.expand),
             ),
-            Text(recipe.name, style: kBoldW600f24Style),
+            Text(widget.recipe.name, style: kBoldW600f24Style),
             SizedBox(
               height: 32,
               child: SimpleStreamBuilder<UserModel>.simpler(
                 context: context,
                 stream: UserFirestoreService().fetchOneStreamFirestore(
-                  recipe.userId,
+                  widget.recipe.userId,
                 ),
                 builder: (user) {
                   return Row(children: [
@@ -112,5 +144,16 @@ class RecipeWidget extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _bookMarkAction() async {
+    try {
+      if (widget.recipe.savedUsersIds.contains(_userId)) {
+        widget.recipe.savedUsersIds.remove(_userId);
+      } else {
+        widget.recipe.savedUsersIds.add(_userId);
+      }
+      await RecipeFirestoreService().updateFirestore(widget.recipe);
+    } catch (_) {}
   }
 }
